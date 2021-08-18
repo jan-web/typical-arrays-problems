1aec829a0762467ce913acbe12158a304994cc68
Yan Shatskiy <iam@yanshatskiy.com>
2021-08-18 12:21:18 +0200

    feat: Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if(array.length === 0) {
+
+    return 0;
+  }
+
+  return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if(array.length === 0) {
+
+    return 0;
+  }
+
+  return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if(array.length === 0) {
+
+    return 0;
+  }
+
+  const sum = array.reduce((acc, elem)=> {
+    return acc+elem;
+  }, 0);
+  const aver = sum / (array.length);
+
+  return aver;
 }
